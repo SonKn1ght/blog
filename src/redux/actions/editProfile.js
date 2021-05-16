12e1607d84e27e2f4de4cde6f0168b481cb340c1
Nikrogan <taifun22113@gmail.com>
2021-05-16 22:40:31 +0300

    fix edit

--- src/redux/actions/editProfile.js
+++ src/redux/actions/editProfile.js
@@ -3,13 +3,18 @@ import {baseUrl} from "../api";
 
 export const editProfile = (date) => {
   return async (dispatch) => {
+    const users = {
+      user:{
+        ...date
+      }
+    }
     const newUserDate = await (await fetch(`${baseUrl}/user`,{
       method: "PUT",
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
         Authorization: `Token ${localStorage.getItem('token')}`
       },
-      body: JSON.stringify(date)
+      body: JSON.stringify(users)
     })).json()
     dispatch({
       type: "POST/AUTHORIZATION",
